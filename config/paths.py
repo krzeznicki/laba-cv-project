23c3a79392606595fe5695f7055cf619e29a8fd2
MikolajRusin <rusinmiko@gmail.com>
2025-02-05 23:15:16 +0100

    Image annotating completed.

--- config/paths.py
+++ config/paths.py
@@ -5,12 +5,14 @@ from pathlib import Path
 HOME = Path(__file__).resolve().parent.parent  # Moves up to FOOTBALLAI_ENDPROJECT
 
 # Define paths
-DATA = HOME / 'data'
+DATA = HOME / 'data'  # Path where the data sets will be saved
+MODELS = HOME / 'Models'  # The path in wich the models are located
 
 # Store paths in a dictionary
 paths = {
     'HOME': HOME,
-    'DATA': DATA
+    'DATA': DATA,
+    'MODELS': MODELS
 }
 
 for path in paths.values():
